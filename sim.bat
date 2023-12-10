dc8564c08b43beb667c7cb9409c8110bfbe86dee
Basil Hussain <basilhussain@users.noreply.github.com>
2023-12-10 11:41:34 +0000

    Update uCsim executable file name

--- sim.bat
+++ sim.bat
@@ -1,4 +1,2 @@
 @echo off
-sstm8.exe -t STM8S208 -X 16M -I if=rom[0x5800] -C "sim_cmds.txt" "bin\Test\test"
-rem sstm8.exe -t STM8S208 -X 16M -S uart=1,port=10000 -I if=rom[0x5800] "bin\Test\test"
-rem start "PuTTY" putty.exe -telnet -P 10000 localhost
+ucsim_stm8.exe -t STM8S208 -X 16M -I if=rom[0x5800] -C "sim_cmds.txt" "bin\Test\test"
